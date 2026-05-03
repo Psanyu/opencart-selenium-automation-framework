295a1d0c8fbdb2e9008dd41dbe96f3e607a82297
Psanyu <psanyogita32@gmail.com>
2026-05-02 17:33:09 -0700

    Update APIt03CreateSalesOrder.java

--- src/test/java/APItesting/APIt03CreateSalesOrder.java
+++ src/test/java/APItesting/APIt03CreateSalesOrder.java
@@ -2,12 +2,14 @@ package APItesting;
 
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
-
 public class APIt03CreateSalesOrder {
+
     @Test
     public void verifyCreateSalesOrderApi() {
 
@@ -21,17 +23,23 @@ public class APIt03CreateSalesOrder {
 
         String sessionId = loginResponse.getCookie("sid");
 
+        System.out.println("Login Status Code: " + loginResponse.statusCode());
+        System.out.println("Session ID: " + sessionId);
+
         String payload = """
                 {
                   "data": {
-                    "customer": "OpenCart Test Customer",
+                    "customer": "Grant Plastics Ltd.",
+                    "company": "XXXX (Demo)",
                     "transaction_date": "2026-04-23",
                     "delivery_date": "2026-04-25",
                     "items": [
                       {
-                        "item_code": "MacBook-Test",
+                        "item_code": "SKU001",
                         "qty": 1,
-                        "rate": 500
+                        "rate": 500,
+                        "warehouse": "Stores - XD",
+                        "delivery_date": "2026-04-25"
                       }
                     ]
                   }
@@ -46,11 +54,26 @@ public class APIt03CreateSalesOrder {
         .when()
                 .post("/api/resource/Sales Order");
 
-        salesOrderResponse.then()
-        .statusCode(200)
-        .body("data.name", notNullValue())
-        .body("data.customer", notNullValue());
-
+        System.out.println("Sales Order Status Code: " + salesOrderResponse.statusCode());
+        System.out.println("Sales Order Response Body:");
         System.out.println(salesOrderResponse.asPrettyString());
+
+        salesOrderResponse.then()
+                .statusCode(200)
+                .body("data.name", notNullValue())
+                .body("data.customer", equalTo("Grant Plastics Ltd."));
+
+        String orderId = salesOrderResponse.jsonPath().getString("data.name");
+        System.out.println("Created Order: " + orderId);
+
+        given()
+                .baseUri("http://localhost:8000")
+                .cookie("sid", sessionId)
+        .when()
+                .get("/api/resource/Sales Order/" + orderId)
+        .then()
+                .statusCode(200)
+                .body("data.name", equalTo(orderId))
+                .body("data.customer", equalTo("Grant Plastics Ltd."));
     }
 }
